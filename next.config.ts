fa11b6b60021849034652ac190360302e5eece51
Kasper Nilsson <kaspnilsson@gmail.com>
2025-06-22 18:02:30 -0400

    Basic landing page with links

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+import { links } from "./src/lib/links";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return links.map((link) => ({
+      source: `/${link.shortText}`,
+      destination: link.href,
+      permanent: true,
+    }));
+  },
 };
 
 export default nextConfig;
---
+++ src/lib/links.ts
@@ -0,0 +1,38 @@
+export interface LinkInfo {
+  // The text is used for the button text
+  text: string;
+  // The short text is used for the URL path
+  shortText: string;
+  // The href is used for the button link
+  href: string;
+}
+
+// Order is the order of the buttons
+export const links: LinkInfo[] = [
+  {
+    text: "Releases",
+    shortText: "releases",
+    href: "https://soundcloud.com/bk-loves-recs/sets/bk-loves-recs-releases",
+  },
+  {
+    text: "Links",
+    shortText: "links",
+    href: "https://linktr.ee/bklovesrecs",
+  },
+  {
+    text: "Contact",
+    shortText: "contact",
+    // TODO: Replace with actual contact page
+    href: "https://www.instagram.com/bklovesrecs/",
+  },
+  {
+    text: "Instagram",
+    shortText: "instagram",
+    href: "https://www.instagram.com/bklovesrecs/",
+  },
+  {
+    text: "Discord",
+    shortText: "discord",
+    href: "https://discord.gg/umxmvfVG9Z",
+  },
+];
