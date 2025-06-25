89565afc57feb83c421634a13e5135ec9e35e8f9
Kasper Nilsson <kaspnilsson@gmail.com>
2025-06-25 21:38:47 +0000

    Fix lint error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import { ThemedLogo } from "@/components/themed-logo";
 import { Button } from "@/components/ui/button";
 import { links } from "@/lib/links";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 sm:p-8">
-        <ThemedLogo />
+      <ThemedLogo />
       <div className="flex w-full max-w-2xl flex-col gap-4 sm:flex-row justify-center">
         {links.map((link) => (
           <Button key={link.text} asChild size="xl" variant="outline">
